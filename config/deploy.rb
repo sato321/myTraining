19928db95d04be0360f07e0aa0aed033176782ac
sato321 <sticky0928fingers@yahoo.ne.jp>
2017-02-21 14:58:36 +0900

    mention what your change about

---
+++ config/deploy.rb
@@ -0,0 +1,89 @@
+require 'mina/rails'
+require 'mina/git'
+# require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
+# require 'mina/rvm'    # for rvm support. (https://rvm.io)
+
+# Basic settings:
+#   domain       - The hostname to SSH to.
+#   deploy_to    - Path to deploy into.
+#   repository   - Git repo to clone from. (needed by mina/git)
+#   branch       - Branch name to deploy. (needed by mina/git)
+
+set :user, 'shunsuke'
+set :domain, '10.0.2.2'
+set :port, '3022'
+set :deploy_to, '/var/www/html'
+set :keep_releases, 1
+set :rails_env, ENV['RAILS_ENV'] || 'production'
+set :shared_dirs, ['log']
+set :shared_files, ['.env']
+ 
+set :repository, 'https://github.com/sato321/myTraining.git'
+set :branch, 'master'
+ 
+set :execution_mode, :system
+
+# Optional settings:
+#   set :user, 'foobar'          # Username in the server to SSH to.
+#   set :port, '30000'           # SSH port number.
+#   set :forward_agent, true     # SSH forward_agent.
+
+# shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
+# set :shared_dirs, fetch(:shared_dirs, []).push('somedir')
+# set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# This task is the environment that is loaded for all remote run commands, such as
+# `mina deploy` or `mina rake`.
+task :environment do
+  command %[source ˜/.bash_profile]
+  command %[rvm use 'ruby-2.3.1']
+  # If you're using rbenv, use this to load the rbenv environment.
+  # Be sure to commit your .ruby-version or .rbenv-version to your repository.
+  # invoke :'rbenv:load'
+
+  # For those using RVM, use this to load an RVM version@gemset.
+  # invoke :'rvm:use', 'ruby-1.9.3-p125@default'
+end
+
+# Put any custom commands you need to run at setup
+# All paths in `shared_dirs` and `shared_paths` will be created on their own.
+task :setup1 do
+command %[sudo mkdir /var/www]
+command %[sudo mkdir /var/www/html]
+command %[sudo chown shunsuke /var/www/html]
+command %[mkdir -p "fetch(:current_path)/shared/log"]
+command %[chmod g+rx,u+rwx "fetch(:current_path)/shared/log"]
+ 
+command %[touch "fetch(:current_path)/shared/.env"]
+  # command %{rbenv install 2.3.0}
+end
+
+desc "Deploys the current version to the server."
+task :deploy do
+  # uncomment this line to make sure you pushed your local branch to the remote origin
+  # invoke :'git:ensure_pushed'
+  deploy do
+    # Put things that will set up an empty directory into a fully set-up
+    # instance of your project.
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+    #invoke :'rails:db_migrate'
+    invoke :'rails:assets_precompile'
+    invoke :'deploy:cleanup'
+
+    on :launch do
+      in_path(fetch(:current_path)) do
+        command %{mkdir -p tmp/}
+        command %{touch tmp/restart.txt}
+      end
+    end
+  end
+
+  # you can use `run :local` to run tasks on local machine before of after the deploy scripts
+  # run(:local){ say 'done' }
+end
+
+# For help in making your deploy script, see the Mina documentation:
+#
+#  - https://github.com/mina-deploy/mina/tree/master/docs
