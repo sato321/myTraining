97f54316856f393743ca80143b883709777a8b7f
sato321 <sticky0928fingers@yahoo.ne.jp>
2017-02-28 15:51:13 +0900

    training3 edit & destroy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,10 +2,12 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
   def new
     @item = Item.new
-    @items = Item.all
+    #@items = Item.all
   end
+
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -14,6 +16,30 @@ class ItemsController < ApplicationController
       render :action => "new"
     end
   end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update_attributes(item_params)
+      redirect_to :action => "index"
+    else
+      render :action => "edit"
+    end
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to :action => "index"
+  end
+
   def item_params
     params.require(:item).permit(:name, :price, :description, :img_url)
   end
